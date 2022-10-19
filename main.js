a45810de332c8f07faa28fa12c9d60d6fe3a8702
quangtriarena <108337222+quangtriarena@users.noreply.github.com>
2022-10-20 00:03:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,5 @@ const token2 = {
 // localStorage.setItem("token2 name ", JSON.stringify(token2));
 
 let data = JSON.parse(localStorage.clear("token2 name "));
+console.log(123)
 console.log(data);
