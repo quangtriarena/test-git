b81e5e523fe530cf9bb497fce63739039fd28d9b
luuquangtri95 <luuquangtri95@gmail.com>
2022-10-20 00:06:58 +0700

    test code

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ const token2 = {
 // localStorage.setItem("token2 name ", JSON.stringify(token2));
 
 let data = JSON.parse(localStorage.clear("token2 name "));
-console.log(123)
+console.log("abc");
 console.log(data);
 console.log(244);
