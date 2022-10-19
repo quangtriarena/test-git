12a9611661f8362276ff0d22a71842796aa523e1
luuquangtri95 <luuquangtri95@gmail.com>
2022-10-19 22:57:40 +0700

    up code

---
+++ main.js
@@ -0,0 +1,13 @@
+const token = "abc" + Math.floor(Math.random() * 100000000000);
+
+const token2 = {
+  name: token,
+};
+
+// console.log(JSON.stringify(token2));
+// move token to localstorage
+
+// localStorage.setItem("token2 name ", JSON.stringify(token2));
+
+let data = JSON.parse(localStorage.clear("token2 name "));
+console.log(data);
